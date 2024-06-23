4496072ceae79a950cec7b3a9eff403ca4d50c9b
Berkay <info@berkaysenkazan.com>
2024-06-23 17:42:03 +0300

    */

---
+++ App.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, (255,255,255), (250,250), 80)
+    pygame.display.flip()
+
+pygame.quit()
